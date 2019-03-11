b8c640f545ae6537fec616c2590a9ea26852195c
Claudia Chira <claudiachira@MBP-van-Claudia.localdomain>
2019-03-11 21:59:12 +0200

    click on menu

--- intro.js
+++ intro.js
@@ -28,3 +28,35 @@ function showPage(page) {
     document.getElementById(page).style.display = "block";
 }
 
+function initMenu() {
+    var links = document.querySelectorAll("#top-menu-bar a");
+    console.info(links);
+    for (var i = 0; i < links.length; i++) {
+    
+    links[i].onclick = clickOnMenuItem;
+    }
+
+    function clickOnMenuItem () {
+
+    console.warn("clicked on menu", this);
+    hideAllPages();
+
+    var pageId = this.getAttribute("data-page")
+    console.warn({pageId});
+    showPage(pageId);
+
+
+  // hide this 
+  // show this page (current page) 
+    }
+}
+    function hideAllPages() {
+    var pages = document.querySelectorAll(".page");
+    for(var i = 0; i< pages.length; i++) {
+    pages[i].style.display ="none";
+}
+
+}
+    initMenu();
+
+
