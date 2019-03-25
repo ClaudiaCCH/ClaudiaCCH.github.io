8dfb6cfaf2d450c200ef114873a6fc78b2d0857b
Claudia Chira <claudiachira@MBP-van-Claudia.localdomain>
2019-03-25 20:10:04 +0200

    move skills to json

--- intro.js
+++ intro.js
@@ -56,13 +56,20 @@ function hideAllPages() {
 initMenu();
 
 function showSkills() {
-    var allEndorsements = [8, 12, 19, 3];
-    var skills = ['html', 'css', 'js', 'nodejs'];
+   // var allEndorsements = [8, 12, 19, 3];
+    //var skills = ['html', 'css', 'js', 'nodejs'];
+
+    var skills = [ 
+        {name: "js", endorsements: 20},
+        {name: "css", endorsements: 12}, 
+        {name: "html", endorsements: 8}, 
+        {name: "nodejs", endorsements: 3}
+    ];
 
 
     var htmlSkills = skills.map(function (skill, index) {
-        var endorsements= '<span class="endorsement"> (' + allEndorsements[index] + ")</span>";
-        return "<li>" + skill.toUpperCase() + endorsements +"</li>";
+        var endorsements= '<span class="endorsement"> (' + skill.endorsements + ")</span>";
+        return "<li>" + skill.name.toUpperCase() + endorsements +"</li>";
     });
 
 
